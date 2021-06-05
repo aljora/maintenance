b939195afc8e8b17b8d918862f0d138824196897
Alexander Ratelle <alexander.ratelle@gmail.com>
2021-06-05 00:38:33 -0400

    Added MonthField to handle typical inputs for service activity intervals

--- maintenance/auto/forms.py
+++ maintenance/auto/forms.py
@@ -1,12 +1,36 @@
-from .models import Inspection, InspectService, Replacement
+import datetime
+from .models import Inspection, InspectService, Replacement, DAYSPERMONTH
+from django.core.exceptions import ValidationError
+from django.forms import DurationField
 from django.forms import ModelForm
 
 
+class MonthField(DurationField):
+    def to_python(self, value):
+        print('HERE')
+        if value in self.empty_values:
+            return None
+        if isinstance(value, datetime.timedelta):
+            return value
+        try:
+            value = datetime.timedelta(days=int(value)*DAYSPERMONTH)
+        except OverflowError:
+            raise ValidationError(self.error_messages['overflow'].format(
+                min_days=datetime.timedelta.min.days,
+                max_days=datetime.timedelta.max.days,
+            ), code='overflow')
+        if value is None:
+            raise ValidationError(self.error_messages['invalid'], code='invalid')
+        return value
+
+
 class InspectionForm(ModelForm):
+    duration = MonthField()
+
+
     class Meta:
         model = Inspection
         fields = '__all__'
-        exclude = ['duration']
 
 
 class ReplacementForm(ModelForm):
